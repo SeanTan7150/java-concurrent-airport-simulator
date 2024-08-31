82cd9f88495bc02b1ac54e48c498c79c28d9a0bc
SeanTan7150 <xiuhawtan@gmail.com>
2024-08-31 11:55:32 +0800

    modify build from netbeans to vscode

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # java-concurrent-airport-simulator
+
+### Build
+
+```shell
+ant compile
+ant jar
+```
+
+### Run
+
+```shell
+ant run
+```
--- src/test/ATC.java
+++ src/test/ATC.java
@@ -8,20 +8,20 @@ public class ATC {
     private static final int AIRPORT_CAP = 3;
     private static final int MAX_PASSENGERS = 50;
     private static final int FUEL_TRUCK_CAP = 1;
-    
+
     private static final Semaphore airportSemaphore = new Semaphore(AIRPORT_CAP);
     public static final Semaphore runwaySemaphore = new Semaphore(RUNWAY_CAP);
     public static final Semaphore fuelTruckSemaphore = new Semaphore(FUEL_TRUCK_CAP);
-    
+
     public static final Gate gateA = new Gate("A");
     public static final Gate gateB = new Gate("B");
     public static final Gate gateC = new Gate("C");
-    
+
     public static void main(String[] args) throws InterruptedException {
         FinalReport.startTotalExecutionTime = System.currentTimeMillis();
         Runway runway = new Runway(airportSemaphore, runwaySemaphore);
         FuelTruck fuelTruck = new FuelTruck(fuelTruckSemaphore);
-        PlaneGenerator planeGenerator = new PlaneGenerator(runway, NUMBER_OF_PLANES, 
+        PlaneGenerator planeGenerator = new PlaneGenerator(runway, NUMBER_OF_PLANES,
                 fuelTruck, MAX_PASSENGERS);
         planeGenerator.generate();
     }
